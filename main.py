c5cad3b4098aa8184a437c38c9ae4f7d2011cbf9
Eli <eli@pluto.local>
2024-01-17 14:48:27 -0500

    added prompting functionality, needs better error checking/handling

--- main.py
+++ main.py
@@ -12,12 +12,22 @@ if __name__ == "__main__":
     while (invalid_uID):
         os.system('cls||clear')
 
+        # get uID of user to copy playlists from
         user_to_copy_from = input("Enter valid user ID you would like to copy public playlists from: ")
-        check_copy_success = publicAccess.copy_playlists(user_to_copy_from)
+
+        # ask user if they want prompting
+        prompt_copy = input("Would you like to be prompted before copying a playlist? [Y/n] ")        
+        prompt_before_copy = False
+        if (prompt_copy.lower() == 'y'):
+            prompt_before_copy = True       
+
+        # call copy playlists with or without prompting
+        check_copy_success = publicAccess.copy_playlists(user_to_copy_from, prompt_before_copy)
+
+        # check if uID passed exists
         if (check_copy_success):
             invalid_uID = False
         else:
             print("\nPlease make sure the uID you are entering is correct...")
             print("Try again in 5 seconds...")
             [time.sleep(1) for i in range(5)]
-
